58159ede53f1dc05efd6a0ecefa8d67626473c2d
Patryk Siuda <jackiehabbit@gmail.com>
2023-06-30 13:56:47 +0200

    Add Prometheus service

---
+++ Prometheus/src/test/kotlin/com/siriusbase/prometheus/PrometheusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.siriusbase.prometheus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrometheusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
