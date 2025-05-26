bf45c2938e0faf3449661c22373dbe73f26ec189
Patryk Siuda <patryksiudaprogrammer@gmail.com>
2025-05-26 15:03:41 +0200

    ECM-1 Added Vaadin code to Order service

--- Order/src/main/java/com/siriusbase/order/OrderApplication.java
+++ Order/src/main/java/com/siriusbase/order/OrderApplication.java
@@ -3,6 +3,8 @@ package com.siriusbase.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,10 @@ public class OrderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
+
+		Pageable pageable = PageRequest.of(0, 10);
+		System.out.println("Page size: " + pageable.getPageSize());
+
 	}
 
 }
